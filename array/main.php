3baab67ec257c8ec8c9175ddf532157de8632238
ssaniah <2223614.saniah@smkn-2sbg.sch.id>
2024-02-19 20:41:23 +0700

    upload file

--- array/main.php
+++ array/main.php
@@ -1,15 +1,125 @@
 <!--Array merupakan kumpulan tipe data dalam suatu program. 
 Array memiliki suatu fungsi sebagai wadah berkumpulnya tipe data yang sama,
  namun dengan elemen yang berbeda.|saniah XI-rpl--> 
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-<style>
+	<head>
+
+		<meta charset="UTF-8">
+		<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="Description" content="Bootstrap Responsive Admin Web Dashboard HTML5 Template">
+		<meta name="Author" content="Spruko Technologies Private Limited">
+		<meta name="Keywords" content="admin,admin dashboard,admin dashboard template,admin panel template,admin template,admin theme,bootstrap 4 admin template,bootstrap 4 dashboard,bootstrap admin,bootstrap admin dashboard,bootstrap admin panel,bootstrap admin template,bootstrap admin theme,bootstrap dashboard,bootstrap form template,bootstrap panel,bootstrap ui kit,dashboard bootstrap 4,dashboard design,dashboard html,dashboard template,dashboard ui kit,envato templates,flat ui,html,html and css templates,html dashboard template,html5,jquery html,premium,premium quality,sidebar bootstrap 4,template admin bootstrap 4"/>
+
+		<!-- Title -->
+		<title> Azira -  Premium dashboard ui bootstrap rwd admin html5 template </title>
+
+		<!--- Favicon --->
+		<link rel="icon" href="../assets/img/brand/favicon.png" type="image/x-icon"/>
+
+		<!-- Bootstrap css -->
+		<link href="../assets/plugins/bootstrap/css/bootstrap.css" rel="stylesheet" id="style"/>
+
+		<!--- Icons css --->
+		<link href="../assets/css/icons.css" rel="stylesheet">
+
+		<!--- Style css --->
+		<link href="../assets/css/style.css" rel="stylesheet">
+		<link href="../assets/css/plugins.css" rel="stylesheet">
+
+		<!--- Animations css --->
+		<link href="../assets/css/animate.css" rel="stylesheet">
+
+	</head>
+
+	<body class="main-body app sidebar-mini ltr">
+
+		<!-- Loader -->
+		<div id="global-loader">
+			<img src="../assets/img/loaders/loader-4.svg" class="loader-img" alt="Loader">
+		</div>
+		<!-- /Loader -->
+
+		<!-- page -->
+	   <div class="page custom-index">
+			<!-- main-sidebar-->
+			<div class="app-sidebar__overlay" data-bs-toggle="sidebar"></div>
+			<div class="sticky">
+				<aside class="app-sidebar sidebar-scroll">
+					<div class="main-sidebar-header active">
+						<a class="desktop-logo logo-light active" href="index.html"><img src="../assets/img/brand/logo.png" class="main-logo" alt="logo"></a>
+						<a class="desktop-logo logo-dark active" href="index.html"><img src="../assets/img/brand/logo-white.png" class="main-logo" alt="logo"></a>
+						<a class="logo-icon mobile-logo icon-light active" href="index.html"><img src="../assets/img/brand/favicon.png" alt="logo"></a>
+						<a class="logo-icon mobile-logo icon-dark active" href="index.html"><img src="../assets/img/brand/favicon-white.png" alt="logo"></a>
+					</div>
+					<div class="main-sidemenu">
+						<div class="main-sidebar-loggedin">
+							<div class="app-sidebar__user">
+								<div class="dropdown user-pro-body text-center">
+									<div class="user-pic">
+										<img src="aku.jpg" alt="user-img" class="rounded-circle mCS_img_loaded">
+									</div>
+									<div class="user-info">
+										<h6 class=" mb-0 text-dark">saniah</h6>
+										<span class="text-muted app-sidebar__user-name text-sm">XI software engineering</span>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="sidebar-navs">
+							<ul class="nav  nav-pills-circle">
+								<li class="nav-item" data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="Settings" aria-describedby="tooltip365540">
+									<a class="nav-link text-center m-2">
+										<i class="fe fe-settings"></i>
+									</a>
+								</li>
+								<li class="nav-item" data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="Chat" aria-describedby="tooltip143427">
+									<a class="nav-link text-center m-2">
+										<i class="fe fe-mail"></i>
+									</a>
+								</li>
+								<li class="nav-item" data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="Followers">
+									<a class="nav-link text-center m-2">
+										<i class="fe fe-user"></i>
+									</a>
+								</li>
+								<li class="nav-item" data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="Logout">
+									<a class="nav-link text-center m-2">
+										<i class="fe fe-power"></i>
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div class="slide-left disabled" id="slide-left"><svg xmlns="http://www.w3.org/2000/svg" fill="#7b8191" width="24" height="24" viewBox="0 0 24 24"><path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"/></svg></div>
+						<ul class="side-menu ">
+							<li class="slide">
+								<a class="side-menu__item" href="dasboard.html"><i class="side-menu__icon fe fe-airplay"></i><span class="side-menu__label">Dashboard</span></a>
+							</li>
+							<!-- <li class="slide">
+								<a class="side-menu__item" data-bs-toggle="slide"   href="jenis buku.html"><i class="side-menu__icon fe fe-box"></i><span class="side-menu__label">jenis buku</span><i class="angle fe fe-chevron-down"></i></a>
+								</ul>
+							</li> -->
+						<div class="slide-right" id="slide-right"><svg xmlns="http://www.w3.org/2000/svg" fill="#7b8191" width="24" height="24" viewBox="0 0 24 24"><path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"/></svg></div>
+					</div>
+				</aside>
+			</div>
+			<!-- main-sidebar -->
+
+			<!-- main-content -->
+			<div class="main-content app-content">
+
+				<!-- container -->
+				<div class="main-container d-flex">
+					<!-- row -->
+					<div class="row mx-auto">
+						<div class="col-12 mt-4 col-mb-4">
+							<div class="card bg-white bd-teal bd-4 rounded-30 ">
+								<div class="item7-card">
+									<a href="javascript:void(0);"></a>
+									<img src="" alt="" height="50" class="cover-image rounded-7 w-auto">
+								</div>
+ <style>
     body{
         background-image: url("pastel.jpg");
         background-color:blue;
@@ -17,8 +127,8 @@ Array memiliki suatu fungsi sebagai wadah berkumpulnya tipe data yang sama,
     tr{
         background-color:pink;
     }
-   </style> 
-  <center>
+   </style>
+    <center class="card-header">
     <table border="2" style="text-align: center; vertical-align: center;">
             <tr>                
                 <td>Foto Siswa</td>
@@ -38,12 +148,88 @@ Array memiliki suatu fungsi sebagai wadah berkumpulnya tipe data yang sama,
             </tr>
     </table>
     </center>
-<center>
+                                     <div class="">
+					                 <a href="javascript:void(0);" class="mt-4"><h5 class="font-weight-semibold text-dark"></h5></a>
+					                <p class=""></p>
+					                <p class="mb-0"></p>
+                                    <center>
+<?php
+$cars = array("Volvo", "BMW", "Toyota"); 
+
+var_dump($cars);
+?>  
+</center><center>
 <?php
 $cars = array("Volvo", "BMW", "Toyota"); 
 
 var_dump($cars);
 ?>  
 </center>
+                                     </div>
+			                      </div>
+		                          </div>
+		                     </div>
+			             </div>
+			         </div>
+					 <!--- Back-to-top --->
+					 <a href="#top" id="back-to-top"><i class="las la-angle-double-up"></i></a>
+								     </div>
+
+<!--- JQuery min js --->
+<script src="../assets/plugins/jquery/jquery.min.js"></script>
+
+<!--- Datepicker js --->
+<script src="../assets/plugins/jquery-ui/ui/widgets/datepicker.js"></script>
+
+<!--- Bootstrap Bundle js --->
+<script src="../assets/plugins/bootstrap/popper.min.js"></script>
+<script src="../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+
+<!--- Ionicons js --->
+<script src="../assets/plugins/ionicons/ionicons.js"></script>
+
+<!--- Moment js --->
+<script src="../assets/plugins/moment/moment.js"></script>
+
+<!--- JQuery sparkline js --->
+<script src="../assets/plugins/jquery-sparkline/jquery.sparkline.min.js"></script>
+
+<!--- Select2 js --->
+<script src="../assets/plugins/select2/js/select2.min.js"></script>
+
+<!--- P-scroll js --->
+<script src="../assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+<script src="../assets/plugins/perfect-scrollbar/p-scroll.js"></script>
+
+<!--- Eva-icons js --->
+<script src="../assets/js/eva-icons.min.js"></script>
+
+<!--- Sidebar js --->
+<script src="../assets/plugins/side-menu/sidemenu.js"></script>
+
+<!--- sticky js --->
+<script src="../assets/js/sticky.js"></script>
+
+<!--- Right-sidebar js --->
+<script src="../assets/plugins/sidebar/sidebar.js"></script>
+<script src="../assets/plugins/sidebar/sidebar-custom.js"></script>
+
+<!--- Internal Gallery js --->
+<script src="../assets/plugins/gallery/lightgallery-all.min.js"></script>
+<script src="../assets/plugins/gallery/jquery.mousewheel.min.js"></script>
+<script src="../assets/js/gallery.js"></script>
+
+
+<!--- Index js --->
+<script src="../assets/js/script.js"></script>
+
+<!--themecolor js-->
+<script src="../assets/js/themecolor.js"></script>
+
+<!--swither-styles js-->
+<script src="../assets/js/swither-styles.js"></script>
+
+<!--- Custom js --->
+<script src="../assets/js/custom.js"></script>
 </body>
 </html>
